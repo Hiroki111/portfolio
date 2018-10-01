35247f4352411f2e99b35753f6bc9951ee0437c4
HyphenDev@HYP-Linux01 <htakahashi@hyphenio.com.au>
2018-10-01 18:07:13 +1000

    Removed console.log

--- js/main.js
+++ js/main.js
@@ -1,9 +1,7 @@
 $(window).scroll(function() {
 	if ($('.navbar').offset().top > 50) {
-		console.log('if', $('.navbar').offset().top);
 		$('.fixed-top').addClass('nav-collapse');
 	} else {
-		console.log('else', $('.navbar').offset().top);
 		$('.fixed-top').removeClass('nav-collapse');
 	}
 })
