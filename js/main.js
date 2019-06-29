1010c3446bcb61eb901506478048eeaed7f3a29f
Hiroki 111 <mitsuke1116@gmail.com>
2019-06-29 06:42:05 +0200

    Updated - June 2019

--- js/main.js
+++ js/main.js
@@ -1,28 +1,5 @@
-$(window).scroll(function() {
-	if ($('.navbar').offset().top > 50) {
-		$('.fixed-top').addClass('nav-collapse');
-	} else {
-		$('.fixed-top').removeClass('nav-collapse');
-	}
-})
-
-$(function() {
-	$('.page-scroll a').bind('click', function() {
-		var $anchor = $(this);
-		$('html, body').stop().animate({
-			scrollTop: $($anchor.attr('href')).offset().top
-		}, 1500, 'easeInOutExpo');
-		event.preventDefault();
-	});
-
-	$('.learn-more-btn').bind('click', function() {
-		var imgSrc = $(this).closest(".work-container").find('.work-img').attr('src');
-		$('#modal-img').attr("src", imgSrc);
-
-		var description = $(this).closest(".work-hover-div").find('.work-description').clone();
-		$('#modal-work-description').html(description);
-
-		var url = $(this).closest(".work-hover-div").find('.site-url').attr('data-url');
-		$('#modal-site-url').attr("href", url);
-	});
-});
+$(window).scroll(function() { $(".navbar").offset().top > 50 ? $(".fixed-top").addClass("nav-collapse") : $(".fixed-top").removeClass("nav-collapse") }), $(function() { $(".page-scroll a").bind("click", function() { var t = $(this);
+		$("html, body").stop().animate({ scrollTop: $(t.attr("href")).offset().top }, 1500, "easeInOutExpo"), event.preventDefault() }), $(".learn-more-btn").bind("click", function() { var t = $(this).closest(".work-container").find(".work-img").attr("src");
+		$("#modal-img").attr("src", t); var o = $(this).closest(".work-hover-div").find(".work-description").clone();
+		$("#modal-work-description").html(o); var r = $(this).closest(".work-hover-div").find(".site-url").attr("data-url");
+		$("#modal-site-url").attr("href", r) }) });
